068be5320db1145fdf4babc5a642978a1f66a03c
Shiatzy Nicole Dulay <dshiatzy@gmail.com>
2026-04-17 20:30:02 +0800

    Connect frontend to live backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,23 +1,13 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
 });
 
-instance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-
-    if (token) {
-      config.headers = config.headers || {};
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
 export default instance;
